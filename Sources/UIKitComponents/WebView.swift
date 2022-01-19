951a05a9d7edbf8ce20b0ad9a2b17956f4233a43
tigi44 <onlytg@naver.com>
2022-01-19 14:35:59 +0900

    add a webview with a navigation header

--- Sources/UIKitComponents/WebView.swift
+++ Sources/UIKitComponents/WebView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 import WebKit
 
+
+
+// MARK: - WebView
+
+
 public struct WebView: View {
     let url: String
     let preferredContentMode: WKWebpagePreferences.ContentMode
@@ -18,13 +23,95 @@ public struct WebView: View {
     }
     
     public var body: some View {
-        UIWebView(url: url, preferredContentMode: preferredContentMode)
+        UIWebView(url: url,
+                  preferredContentMode: preferredContentMode,
+                  webViewObject: ObserableWebView())
     }
+}
+
+
+// MARK: - NaviWebView - with web navigation header
+
+
+public struct NaviWebView: View {
+    let url: String
+    let preferredContentMode: WKWebpagePreferences.ContentMode
+    
+    @State private var canGoBack: Bool = false
+    @State private var canGoForward: Bool = false
+    @StateObject private var webViewObject: ObserableWebView = ObserableWebView()
+    
+    public init(url: String, preferredContentMode: WKWebpagePreferences.ContentMode = .mobile) {
+        self.url = url
+        self.preferredContentMode = preferredContentMode
+    }
+    
+    public var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                HStack(spacing: 20) {
+                    Button {
+                        webViewObject.webView?.goBack()
+                    } label: {
+                        Image(systemName: "chevron.backward")
+                    }
+                    .disabled(!(webViewObject.webView?.canGoBack ?? false))
+                    
+                    Button {
+                        webViewObject.webView?.goForward()
+                    } label: {
+                        Image(systemName: "chevron.forward")
+                    }
+                    .disabled(!(webViewObject.webView?.canGoForward ?? false))
+                }
+                
+                Spacer()
+                
+                Button {
+                    if let url = webViewObject.webView?.url {
+                        print(url)
+                        UIApplication.shared.open(url,
+                                                  options: [:],
+                                                  completionHandler: nil)
+                    }
+                } label: {
+                    Image(systemName: "safari")
+                }
+            }
+            .padding()
+            .background(.regularMaterial)
+            
+            UIWebView(url: url,
+                      preferredContentMode: preferredContentMode,
+                      webViewObject: webViewObject)
+        }
+    }
+}
+
+
+// MARK: - preview
+
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView(url: "https://www.apple.com")
+        
+        NaviWebView(url: "https://www.apple.com")
+    }
+}
+
+
+// MARK: - UIWebView : UIViewRepresentable
+
+
+class ObserableWebView: ObservableObject {
+    @Published var webView: WKWebView?
 }
 
 struct UIWebView: UIViewRepresentable {
     var url: String
     let preferredContentMode: WKWebpagePreferences.ContentMode
+    @ObservedObject var webViewObject: ObserableWebView
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -73,16 +160,14 @@ struct UIWebView: UIViewRepresentable {
             decisionHandler(.allow)
         }
         
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            parent.webViewObject.webView = webView
+        }
+        
         func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
             webView.load(navigationAction.request)
             
             return nil
         }
-    }
-}
-
-struct WebView_Previews: PreviewProvider {
-    static var previews: some View {
-        WebView(url: "https://www.apple.com")
     }
 }
