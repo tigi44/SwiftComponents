9525f2b0cbb382273fc9c6e8841a62bf45852b80
tigi44 <onlytg@naver.com>
2022-01-25 18:04:13 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,8 +9,11 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftComponents",
-            targets: ["SwiftUIComponents", "FoundationComponents"]),
+            name: "SwiftUIComponents",
+            targets: ["SwiftUIComponents"]),
+        .library(
+            name: "FoundationComponents",
+            targets: ["FoundationComponents"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
